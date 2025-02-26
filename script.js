cb74b65ea292707950a267e1bde93a9df3215839
fakedy <34602297+fakedy@users.noreply.github.com>
2025-02-26 14:56:16 +0100

    invalid character fix

--- script.js
+++ script.js
@@ -13,9 +13,15 @@ function calculate() {
 
 
     for(let i = 0; i < 25; i++){
+        let element = document.createElement("p");
 
         for(let c = 0; c < usedText.length; c++){
             let indexOfChar = alphabet.indexOf(usedText[c]);
+            if(indexOfChar == -1){
+                element.innerText = "Invalid characters in text to be deciphered";
+                outputDiv.appendChild(element);
+                return;
+            }
 
             if(indexOfChar == 0){
                 indexOfChar = alphabet.length-2; // skipping Z when rotating left
@@ -29,7 +35,6 @@ function calculate() {
     
         usedText = shiftedText;
         shiftedText = "";
-        let element = document.createElement("p");
         element.innerText = "shift " + i + ": " + usedText;
         outputDiv.appendChild(element);
     }
